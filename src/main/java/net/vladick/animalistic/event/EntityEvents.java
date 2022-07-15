17aa9688ae1602e3dc843d85a33df5ec4845345f
hamzaalshoky <moozyalshoky@gmail.com>
2022-07-15 09:20:23 +0300

    fix #1

--- src/main/java/net/vladick/animalistic/event/EntityEvents.java
+++ src/main/java/net/vladick/animalistic/event/EntityEvents.java
@@ -30,63 +30,65 @@ public class EntityEvents {
 
     @SubscribeEvent
     public static void onInteractEntity(PlayerInteractEvent.EntityInteract event) {
-        LivingEntity entity = (LivingEntity) event.getTarget();
-        Player player = event.getPlayer();
-        ItemStack stack = event.getItemStack();
-        if (stack.getItem() == Items.WATER_BUCKET && entity.isAlive()) {
-            ItemStack bucket = ItemStack.EMPTY;
-            if (entity.getType() == ModEntityTypes.MUDPUPPY.get()) {
-                bucket = new ItemStack(ModItems.MUDPUPPY_BUCKET.get());
-            } else {
-                return;
+        if (event.getTarget() instanceof LivingEntity) {
+            LivingEntity entity = (LivingEntity) event.getTarget();
+            Player player = event.getPlayer();
+            ItemStack stack = event.getItemStack();
+            if (stack.getItem() == Items.WATER_BUCKET && entity.isAlive()) {
+                ItemStack bucket = ItemStack.EMPTY;
+                if (entity.getType() == ModEntityTypes.MUDPUPPY.get()) {
+                    bucket = new ItemStack(ModItems.MUDPUPPY_BUCKET.get());
+                } else {
+                    return;
+                }
+
+                player.swing(event.getHand());
+                entity.playSound(SoundEvents.BUCKET_FILL_FISH, 1.0F, 1.0F);
+                stack.shrink(1);
+
+                if (entity.hasCustomName()) {
+                    bucket.setHoverName(entity.getCustomName());
+                }
+
+                if (!event.getWorld().isClientSide) {
+                    CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer) player, bucket);
+                }
+
+                if (stack.isEmpty()) {
+                    player.setItemInHand(event.getHand(), bucket);
+                } else if (!player.getInventory().add(bucket)) {
+                    player.drop(bucket, false);
+                }
+
+                entity.discard();
+            }else if (stack.getItem() == Items.WATER_BUCKET && entity.isAlive()) {
+                ItemStack bucket = ItemStack.EMPTY;
+                if (entity.getType() == ModEntityTypes.KRILL.get()) {
+                    bucket = new ItemStack(ModItems.KRILL_BUCKET.get());
+                } else {
+                    return;
+                }
+
+                player.swing(event.getHand());
+                entity.playSound(SoundEvents.BUCKET_FILL_FISH, 1.0F, 1.0F);
+                stack.shrink(1);
+
+                if (entity.hasCustomName()) {
+                    bucket.setHoverName(entity.getCustomName());
+                }
+
+                if (!event.getWorld().isClientSide) {
+                    CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer) player, bucket);
+                }
+
+                if (stack.isEmpty()) {
+                    player.setItemInHand(event.getHand(), bucket);
+                } else if (!player.getInventory().add(bucket)) {
+                    player.drop(bucket, false);
+                }
+
+                entity.discard();
             }
-
-            player.swing(event.getHand());
-            entity.playSound(SoundEvents.BUCKET_FILL_FISH, 1.0F, 1.0F);
-            stack.shrink(1);
-
-            if (entity.hasCustomName()) {
-                bucket.setHoverName(entity.getCustomName());
-            }
-
-            if (!event.getWorld().isClientSide) {
-                CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer) player, bucket);
-            }
-
-            if (stack.isEmpty()) {
-                player.setItemInHand(event.getHand(), bucket);
-            } else if (!player.getInventory().add(bucket)) {
-                player.drop(bucket, false);
-            }
-
-            entity.discard();
-        }else if (stack.getItem() == Items.WATER_BUCKET && entity.isAlive()) {
-            ItemStack bucket = ItemStack.EMPTY;
-            if (entity.getType() == ModEntityTypes.KRILL.get()) {
-                bucket = new ItemStack(ModItems.KRILL_BUCKET.get());
-            } else {
-                return;
-            }
-
-            player.swing(event.getHand());
-            entity.playSound(SoundEvents.BUCKET_FILL_FISH, 1.0F, 1.0F);
-            stack.shrink(1);
-
-            if (entity.hasCustomName()) {
-                bucket.setHoverName(entity.getCustomName());
-            }
-
-            if (!event.getWorld().isClientSide) {
-                CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer) player, bucket);
-            }
-
-            if (stack.isEmpty()) {
-                player.setItemInHand(event.getHand(), bucket);
-            } else if (!player.getInventory().add(bucket)) {
-                player.drop(bucket, false);
-            }
-
-            entity.discard();
         }
     }
 
